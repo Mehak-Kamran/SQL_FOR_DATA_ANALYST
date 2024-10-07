7616cef481dae038e9f131fccc4e7302f6415e18
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-10-07 18:43:01 +0500

    create db ,table,insert into ,select
    Revison of SQL starting from basic queries

---
+++ Season2ofSQL/basic1.sql
@@ -0,0 +1,24 @@
+--1----CREATING DB
+CREATE DATABASE PRACTICE;
+
+--2--CREATING A TABLE
+CREATE TABLE EMPLOYEE_TABLE
+(
+E_ID INT,
+FIRSTNAME varchar(50),
+LASTNAME varchar(50),
+AGE INT,
+GENDER varchar(10)
+);
+--3 show table
+SELECT * FROM EMPLOYEE_TABLE;
+
+--4 adding data in table
+INSERT INTO EMPLOYEE_TABLE VALUES
+(
+10001,
+'john',
+'Doe',
+ 23,
+'Male'
+);
